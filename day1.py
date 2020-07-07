af3d1970ea08f5b32b7eb2f13845e0f30dd3e08d
PatrycjaOwczarek <patrycja.k.owczarek@gmail.com>
2020-07-07 14:51:21 +0200

    Added part 2 of day 1

--- day1.py
+++ day1.py
@@ -4,9 +4,19 @@ with open("inputs/input_day_1.txt") as f:
 print(modules_mass)
 
 def required_fuel(masses):
-    fuel_sum = 0 
-    for mass in masses:
-        fuel_sum += (int(mass/3))-2
-    print(fuel_sum)
+    return sum((int(mass/3))-2 for mass in masses)
 
-required_fuel(modules_mass)
+print(required_fuel(modules_mass))
+
+def required_fuel_p2(masses):
+    """Required fuel changed to part 2 of challange"""
+    fuel_sum = 0
+    fuels = [(int(mass/3))-2 for mass in masses]
+    for fuel in fuels:
+        while fuel > 0:
+            fuel_sum += fuel
+            fuel = (int(fuel/3))-2
+    return fuel_sum
+
+print(required_fuel_p2([1969]))
+print(required_fuel_p2(modules_mass))
