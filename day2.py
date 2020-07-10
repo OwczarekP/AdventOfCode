d0013aa1b35a44034913d9b58732f72ff0db0599
PatrycjaOwczarek <patrycja.k.owczarek@gmail.com>
2020-07-10 13:01:24 +0200

    added day 2 part 2

--- day2.py
+++ day2.py
@@ -1,12 +1,14 @@
 import pytest
+import copy
 
 with open("inputs/input_day_2.txt") as f:
     intcode = [int(x) for x in f.read().split(',')]
 
 
+intcode_copy = copy.deepcopy(intcode)
 intcode[1] = 12
 intcode[2] = 2
-print(intcode)
+
 
 def read_intcode(input):
     for i in range(0, len(input), 4):
@@ -16,16 +18,25 @@ def read_intcode(input):
             input[input[i+3]] =  input[input[i+1]] * input[input[i+2]]
         elif input[i] == 99:
             break
+    return input[0]
+
+
+def find_parameters():
+    for noun in range(100):
+        for verb in range(100):
+            memory = intcode_copy.copy()
+            memory[1] = noun
+            memory[2] = verb
+            if read_intcode(memory) == 19690720:
+                return 100 * noun + verb
+
 
 def test():
     test_intcode = [1, 0, 0, 0, 99]
     test_intcode_2 = [1,1,1,4,99,5,6,0,99]
-    read_intcode(test_intcode) 
-    read_intcode(test_intcode_2)
-    assert test_intcode[0] == 2
-    assert test_intcode_2[0] == 30
-    print(test_intcode_2)
+    assert read_intcode(test_intcode) == 2
+    assert read_intcode(test_intcode_2) == 30
 
 test()
-read_intcode(intcode)
-print(intcode[0])
+print(read_intcode(intcode))
+print(find_parameters())
