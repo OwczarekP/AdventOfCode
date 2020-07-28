bd60251c7e560dd13f670491d03524c52ab0f2b8
PatrycjaOwczarek <patrycja.k.owczarek@gmail.com>
2020-07-28 12:54:34 +0200

    Added 2019 folder

--- 2019/day6.py
+++ 2019/day6.py
@@ -10,11 +10,11 @@ def get_direct_orbits(orbits_data):
 
 def get_all_oribits(objects_dict):
     orbits_sum = 0
-    for orbiter in objects_dict:
-        next_orbit = objects_dict.get(orbiter, None)
+    for orbit in objects_dict:
+        next_orbit = objects_dict.get(orbit)
         while next_orbit:
             orbits_sum += 1
-            next_orbit = objects_dict.get(next_orbit, None)
+            next_orbit = objects_dict.get(next_orbit)
     return orbits_sum
 
 get_direct_orbits(map_data)
