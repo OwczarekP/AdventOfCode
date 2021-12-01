886054f5764bf9e9dbcfa965b4eb8c5c69819c60
Patrycja Owczarek <patrycja.k.owczarek@gmail.com>
2021-12-01 15:04:05 +0100

    Add solution for day 1 part 2

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,11 +1,20 @@
-
 with open('./inputs/day1.txt') as f:
-    depth_list = [int(x) for x in f.read().splitlines()]
+    input_list = [int(x) for x in f.read().splitlines()]
 
 
 def count_increased(depth_list):
-    increases = 0
     increases = sum([1 if (depth_list[i] < depth_list[i+1]) else 0 for i in range(len(depth_list)-1)])
     return increases
 
-print(count_increased(depth_list))
+
+print(count_increased(input_list))
+
+
+#Part two
+def count_increased_sums(depth_list):
+    to_skip = 3-len(depth_list) % 3
+    increases = sum([1 if (sum(depth_list[i:i+3]) < sum(depth_list[i+1:i+4])) else 0 for i in range(0, len(depth_list)-to_skip)])
+    return increases
+
+
+print(count_increased_sums(input_list))
